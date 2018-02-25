8415d21913ab9cb4319fe46a7728bbbeb7e67368
teepiik <teemu.piik@gmail.com>
2018-02-25 11:24:32 +0200

    osa5 alotettu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      blogs: []
+      blogs: [],
+      newBlogTitle: '',
+      newBlogAuthor: '',
+      newBlogUrl: '',
+      error: null,
+      success: null,
+      username: '',
+      password: '',
+      user: null
     }
   }
 
@@ -14,14 +23,73 @@ class App extends React.Component {
     blogService.getAll().then(blogs =>
       this.setState({ blogs })
     )
-  } 
+  }
+
+  // use this for create blog too
+  handleLoginFieldChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  login = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username: this.state.username,
+        password: this.state.password
+      })
+
+      this.setState({ username: '', password: '', user })
+    } catch (exception) {
+      this.setState({
+        error: 'wrong username or password',
+      })
+      setTimeout(() => {
+        this.setState({ error: null })
+      }, 5000)
+    }
+  }
 
   render() {
+
+    if (this.state.user === null) {
+      return (
+        <div>
+          <Notification message={this.state.error} />
+          <Notification message={this.state.success} />
+
+          <h2>Login</h2>
+          <form onSubmit={this.login}>
+            <div>
+              username
+              <input
+                type="text"
+                name="username"
+                value={this.state.username}
+                onChange={this.handleLoginFieldChange}
+              />
+            </div>
+            <div>
+              password
+              <input
+                type="password"
+                name="password"
+                value={this.state.password}
+                onChange={this.handleLoginFieldChange}
+              />
+            </div>
+            <button type="submit">login</button>
+          </form>
+        </div>
+      )
+    }
+
     return (
       <div>
+        <Notification message={this.state.error} />
+        <Notification message={this.state.success} />
         <h2>blogs</h2>
-        {this.state.blogs.map(blog => 
-          <Blog key={blog._id} blog={blog}/>
+        {this.state.blogs.map(blog =>
+          <Blog key={blog._id} blog={blog} />
         )}
       </div>
     );
