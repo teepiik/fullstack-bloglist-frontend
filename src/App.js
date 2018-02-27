75ff182dfdeed9008eb9a7f206d0779b2d7ffaf7
teepiik <teemu.piik@gmail.com>
2018-02-27 16:29:54 +0200

    t.5.1 - 5.4 tehty

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import Notification from './components/Notification'
 
 class App extends React.Component {
   constructor(props) {
@@ -23,6 +24,33 @@ class App extends React.Component {
     blogService.getAll().then(blogs =>
       this.setState({ blogs })
     )
+
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      this.setState({ user })
+      blogService.setToken(user.token)
+    }
+  }
+
+  addBlog = async (event) => {
+    event.preventDefault()
+    const blogObject = {
+      title: this.state.newBlogTitle,
+      author: this.state.newBlogAuthor,
+      url: this.state.newBlogUrl
+    }
+    const newBlog = await blogService.create(blogObject)
+    this.setState({
+      blogs: this.state.blogs.concat(newBlog),
+      newBlogTitle: '',
+      newBlogAuthor: '',
+      newBlogUrl: '',
+      success: 'New blog created'
+    })
+    setTimeout(() => {
+      this.setState({ success: null })
+    }, 5000)
   }
 
   // use this for create blog too
@@ -38,7 +66,12 @@ class App extends React.Component {
         password: this.state.password
       })
 
-      this.setState({ username: '', password: '', user })
+      window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
+      blogService.setToken(user.token)
+      this.setState({ username: '', success: 'Login successfully', password: '', user })
+      setTimeout(() => {
+        this.setState({ success: null })
+      }, 5000)
     } catch (exception) {
       this.setState({
         error: 'wrong username or password',
@@ -49,7 +82,49 @@ class App extends React.Component {
     }
   }
 
+  logout = (event) => {
+    this.setState(this.user = null)
+    window.localStorage.clear()
+  }
+
   render() {
+
+    const blogForm = () => (
+      <div>
+        <h2>Add new blog</h2>
+
+        <form onSubmit={this.addBlog}>
+        <div>
+              title
+              <input
+                type="text"
+                name="newBlogTitle"
+                value={this.state.newBlogTitle}
+                onChange={this.handleLoginFieldChange}
+              />
+            </div>
+            <div>
+              author
+              <input
+                type="text"
+                name="newBlogAuthor"
+                value={this.state.newBlogAuthor}
+                onChange={this.handleLoginFieldChange}
+              />
+            </div>
+            <div>
+              url
+              <input
+                type="text"
+                name="newBlogUrl"
+                value={this.state.newBlogUrl}
+                onChange={this.handleLoginFieldChange}
+              />
+            </div>
+          <button type="submit">create</button>
+        </form>
+      </div>
+    )
 
     if (this.state.user === null) {
       return (
@@ -88,9 +163,14 @@ class App extends React.Component {
         <Notification message={this.state.error} />
         <Notification message={this.state.success} />
         <h2>blogs</h2>
+        <p>{this.state.user.username} is logged in
+          <button onClick={this.logout}>
+            Logout
+          </button></p>
         {this.state.blogs.map(blog =>
           <Blog key={blog._id} blog={blog} />
         )}
+        {blogForm()}
       </div>
     );
   }
